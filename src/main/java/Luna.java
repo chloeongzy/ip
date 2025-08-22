431dc8612c4230a380cf19e7d525ab8d463f01fd
chloeongzy <chloeongzy05@gmail.com>
2025-08-22 19:11:01 +0800

    level 3 completed

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 public class Luna {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> currList = new ArrayList<>();
+        ArrayList<Task> currList = new ArrayList<>();
 
         String greeting = " Hello! I'm Luna\n What can I do for you?";
-        String exit = "Bye. Hope to see you again soon!";
+        String exit = "Byebyeee~~ Hope to see you again soon!";
 
         System.out.println(greeting);
 
@@ -20,8 +20,23 @@ public class Luna {
                 for (int i = 0; i < currList.size(); i++) {
                     System.out.println(" " + (i + 1) + ". " + currList.get(i));
                 }
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (index >= 0 && index < currList.size()) {
+                    currList.get(index).mark();
+                    System.out.println(" Yayyy!! I've marked this task as done: ");
+                    System.out.println("  " + currList.get(index));
+                }
+            }  else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (index >= 0 && index < currList.size()) {
+                    currList.get(index).unmark();
+                    System.out.println(" Oh no :( I've marked this task as not done yet: ");
+                    System.out.println("  " + currList.get(index));
+                }
             } else {
-                currList.add(input);
+                Task newTask = new Task(input);
+                currList.add(newTask);
                 System.out.println(" added: " + input);
             }
         }
