d6cb7d146cb8dba6ab4de1689c88479854eeb371
chloeongzy <chloeongzy05@gmail.com>
2025-08-22 18:07:15 +0800

    level 0 completed

---
+++ src/main/java/Luna.java
@@ -0,0 +1,7 @@
+public class Luna {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Luna\nWhat can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greeting + exit);
+    }
+}
