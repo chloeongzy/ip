503e32366cf76b1af673aa1a395d3cd3d748b66c
chloeongzy <chloeongzy05@gmail.com>
2025-08-22 18:19:44 +0800

    level 1 completed

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Luna {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Luna\nWhat can I do for you?\n";
+        Scanner sc = new Scanner(System.in);
+        String greeting = "Hello! I'm Luna\nWhat can I do for you?";
         String exit = "Bye. Hope to see you again soon!";
-        System.out.println(greeting + exit);
+
+        System.out.println(greeting);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(exit);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        sc.close();
+
     }
 }
