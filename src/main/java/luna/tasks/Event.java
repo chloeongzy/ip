1109363eb7fc02945c42cc3cc7e60dd0062652a3
chloeongzy <chloeongzy05@gmail.com>
2025-08-28 03:39:19 +0800

    level-8: complete

--- src/main/java/luna/tasks/Event.java
+++ src/main/java/luna/tasks/Event.java
@@ -1,23 +1,31 @@
 package luna.tasks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
 
     public Event(String description, boolean isDone, String from, String to) {
         super(description, isDone);
-        this.from = from;
-        this.to = to;
+        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        this.from = LocalDateTime.parse(from, inputFormat);
+        this.to = LocalDateTime.parse(to, inputFormat);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        return "[E]" + super.toString() + " (from: " +
+                this.from.format(outputFormat) + " to: " + this.to.format(outputFormat) + ")";
     }
 
     @Override
     public String toFileString() {
-        return "E" + super.toFileString() + " | " + from + " | " + to;
+        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        return "E" + super.toFileString() + " | " +
+                from.format(outputFormat) + " | " + to.format(outputFormat);
     }
 }
